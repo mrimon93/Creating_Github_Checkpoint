8fed00a3dee81be6cb2958191f47a259a21bc373
Muzammil K <muzammil_kamaly93@live.se>
2022-11-25 14:26:55 +0100

    For Task 4

---
+++  README.md
@@ -0,0 +1,6 @@
+Hello, 
+
+Welcome to the md file project creation
+This is for Task 4 Branching and merging 
+by Muzammil. 
+Hopefully you will read this hehe
---
+++ checkpoint/dict.py
@@ -0,0 +1,57 @@
+import psycopg2
+import time
+
+
+conn = psycopg2.connect(
+   host="localhost",
+   database="Phonelistdv",
+   user="postgres",
+   password="Monasogsql@12"
+)
+
+print("Welcome Put one of these Commands ")
+print("list, add, delete or quit. \n")
+
+
+
+def insert_word(C, word, translation):
+    return f"returning ''{word} and {translation}"
+
+def read_dict(C):
+    cur = C.cursor()
+    cur.execute("SELECT id, word, translation FROM dictionary;")
+    rows = cur.fetchall()
+    cur.close()
+    return rows
+def add_word(C, word, translation):
+    cur = C.cursor()
+    cur.execute(f"INSERT INTO dictionary (word, translation) VALUES ('{word}', '{translation}');")
+    cur.close()
+def delete_word(C, ID):
+    cur = C.cursor()
+    cur.execute(f"DELETE FROM dictionary WHERE id = '{ID}';")
+    cur.close()
+def save_dict(C):
+    cur = C.cursor()
+    cur.execute("COMMIT;")
+    cur.close()
+
+text_file()
+
+while True: ## REPL - Read Execute Program Loop
+    cmd = input("What is your COMMAND your highness : ").lower()
+    if cmd == "list":
+        print(read_dict(conn))
+    elif cmd == "add":
+        name = input("  Word: ")
+        phone = input("  Translation: ")
+        add_word(conn, name, phone)
+        print(f"Word '{name}' and Translation '{phone}' has been added")
+    elif cmd == "delete":
+        ID = input("  ID: ")
+        delete_word(conn, ID)
+    elif cmd == "quit":
+        save_dict(conn)
+        print("See you next time")
+        time.sleep(2)
+        exit()
